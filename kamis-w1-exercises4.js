7a8b0be0064df479e2e4cc57849e68baba31880e
indinabilah <indinabilahaulia@gmail.com>
2019-03-15 22:30:28 +0700

    week1-kamis

--- kamis-w1-exercises4.js
+++ kamis-w1-exercises4.js
@@ -1,50 +1,109 @@
 //kamis-w1-exercises4
-var tanggal = 2
-var bulan = 13
-var tahun = 2000
+var tanggal = 5
+var bulan = 4
+var tahun = 1997
 
 
 switch(bulan){
   case 1:
   bulan = "Januari"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 2:
   bulan = "Februari"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 3:
   bulan = "Maret"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 4:
   bulan = "April"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 5:
   bulan = "Mei"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 6:
   bulan = "Juni"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 7:
   bulan  =  "Juli"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 8:
   bulan = "Agustus"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 9:
   bulan = "September"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 10:
   bulan = "Oktober"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 11:
   bulan = "November"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
   break;
   case 12:
   bulan = "Desember"
+  if(tanggal > 0 && tanggal < 32){
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+  }else{
+    console.log("bulan hanya sampai 12 dan tanggal hanya sampai 31")
+  }
+  break;
+  default:
+  console.log('bulan hanya sampai 12 dan tanggal hanya sampai 31')
   break;
 }
 
-if(bulan <= 12 && tanggal <= 31){
-  console.log(tanggal + ' ' + bulan + ' ' + tahun)
-}else{
-  console.log('bulan hanya sampai 12 dan tanggal hanya sampai 31')
-}
+
